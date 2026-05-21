0ff1cbd0855f87363f9204d9f9f4e0cabfaffe1f
absaralam06-code <absaralam12092006@gmail.com>
2026-05-21 10:14:30 +0530

    Update and rename 2_1 to 2_1.cpp

--- 2_1.cpp
+++ 2_1.cpp
@@ -1,8 +1,8 @@
-# Given a sorted array of positive integers containing few
+/* Given a sorted array of positive integers containing few
 duplicate elements, design an algorithm and implement it using
 a program to find whether the given key element is present in
 the array or not. If present, then also find the number of copies
-of given key. (Time Complexity = O(log n))#
+of given key. (Time Complexity = O(log n))*/
 
 #include <iostream>
 #include <vector>
