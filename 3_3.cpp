02239c7c960a8f1a70ea8ef81dfe55325840f6f9
absaralam06-code <absaralam12092006@gmail.com>
2026-05-21 10:16:14 +0530

    Update and rename 3_3 to 3_3.cpp

--- 3_3.cpp
+++ 3_3.cpp
@@ -1,7 +1,7 @@
-#Given an unsorted array of positive integers, design an
+/*Given an unsorted array of positive integers, design an
 algorithm and implement it using a program to find whether
 there are any duplicate elements in the array or not. (use sorting)
-(Time Complexity = O(n log n))#
+(Time Complexity = O(n log n))*/
 
 #include <iostream>
 #include <vector>
