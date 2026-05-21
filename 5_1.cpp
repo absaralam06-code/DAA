a44273550bf454e28ec3e4135bc7ad7fb98a3f94
absaralam06-code <absaralam12092006@gmail.com>
2026-05-21 10:17:41 +0530

    Update and rename 5_1 to 5_1.cpp

--- 5_1.cpp
+++ 5_1.cpp
@@ -1,8 +1,8 @@
-#Given an unsorted array of alphabets containing duplicate
+/*Given an unsorted array of alphabets containing duplicate
 elements. Design an algorithm and implement it using a
 program to find which alphabet has maximum number of
 occurrences and print it. (Time Complexity = O(n)) (Hint: Use
-counting sort)#
+counting sort)*/
 
 #include <iostream>
 #include<vector>
