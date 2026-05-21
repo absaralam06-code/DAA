97f555470e582b7b24c1599a2452a7c2cb8b17af
absaralam06-code <absaralam12092006@gmail.com>
2026-05-21 10:15:11 +0530

    Update and rename 2_3 to 2_3.cpp

--- 2_3.cpp
+++ 2_3.cpp
@@ -1,6 +1,6 @@
-#Given an array of nonnegative integers, design an algorithm and
+/*Given an array of nonnegative integers, design an algorithm and
 a program to count the number of pairs of integers such that
-their difference is equal to a given key, K.#
+their difference is equal to a given key, K.*/
 
 #include <iostream>
 #include <vector>
