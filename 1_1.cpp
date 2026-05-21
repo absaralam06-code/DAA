83f39be4df552339d678020ea2a44ca63d98495f
absaralam06-code <absaralam12092006@gmail.com>
2026-05-21 10:13:23 +0530

    Update and rename 1_1 to 1_1.cpp

--- 1_1.cpp
+++ 1_1.cpp
@@ -1,8 +1,8 @@
-#Given an array of nonnegative integers, design a linear
+/*Given an array of nonnegative integers, design a linear
 algorithm and implement it using a program to find whether
 given key element is present in the array or not. Also, find total
 number of comparisons for each input case. (Time Complexity
-= O(n), where n is the size of input)#
+= O(n), where n is the size of input)*/
 
 #include <iostream>
 #include<vector>
